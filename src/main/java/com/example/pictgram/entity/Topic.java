4009f8fa5d3c127b620bc5bdfecfa0d687c4e47c
kazuma <kazowl2960@gamil.com>
2023-02-12 01:02:29 +0900

    02/12

--- src/main/java/com/example/pictgram/entity/Topic.java
+++ src/main/java/com/example/pictgram/entity/Topic.java
@@ -11,12 +11,11 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
-
-import javax.persistence.SequenceGenerator;
-import javax.persistence.Table;
 
 import lombok.Data;
 
